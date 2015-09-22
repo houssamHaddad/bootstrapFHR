c322fef83369a5a06a12a09bfd2ac03ca03589a3
Marteel <marteel@sio.stremi>
2015-09-22 18:42:55 +0200

    modif essai 1 :wq

---
+++ bootstrap.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+</head>
+ 
+<body>
+<!--<div class="container-fluid">-->
+	<div class="topbar">
+		<div class="fill">
+			<div class="container">
+				<h3><a href="/">Mon Site</a></h3>
+			</div>	
+		</div>
+	</div>	
+	<div class="container">
+	<!--<div class="content">-->
+	<h1>mon premier bootstrap</h1>
+	<label for="test"> sqdglkjslg</label>
+	<input type="text" name="test" id="test"></br>
+	<!--</div>
+	<div class="sidebar">
+	</div>-->
+	</div>
+</body>
+</html>
